a0e051b2c0e0211ead6a9cfffaaeb4adfaffdd2c
Miguel Torre <m.a.torre.f@gmail.com>
2022-05-17 12:24:25 -0700

    first resolver function done

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,19 @@ const { User } = require ('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 const { Book } = require ('../models');
+
+const resolvers = {
+    Query: {
+      me: async (_parent, _args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password');
+  
+            return userData;
+        }
+        throw new AuthenticationError('You need to be logged in!');
+    }
+    }
+  };
+    
+    module.exports = resolvers;
