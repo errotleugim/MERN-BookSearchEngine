7ed44c29ce87376cd0123b9eb960eb8663312ca6
Miguel Torre <m.a.torre.f@gmail.com>
2022-05-19 08:52:41 -0700

    having some weird server issue, might have to revert to an older version of apollo if I cant debug it in time

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-// const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
+const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware
+// });
 
-server.applyMiddleware({app});
+const server = async function startApolloServer(typeDefs, resolvers){
+  const server = new ApolloServer({typeDefs, resolvers})
+  const app = express();
+  await server.start();
+  server.applyMiddleware({app, path: '/graphql'});
+  
+  app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}${server.graphqlPath}`);
+})
+}
+
+startApolloServer(typeDefs, resolvers);
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,12 +37,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
+
 });
